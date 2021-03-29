b166edfddf1e0eca7c554cb57d719a99384723fc
Zero Interest <busquet.francesc@gmail.com>
2021-03-29 21:25:44 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,24 @@
   const lamp = document.getElementById("mode");
 
   const toggleTheme = (state) => {
-    if (state === "dark") {
+     if (state === "dark") {
       localStorage.setItem("theme", "light");
       body.removeAttribute("data-theme");
+      const message = {
+          type: 'set-theme',
+          theme: 'github-light'
+        };
+        var utterances = document.querySelector('iframe');
+        utterances.contentWindow.postMessage(message, 'https://utteranc.es');
     } else if (state === "light") {
       localStorage.setItem("theme", "dark");
       body.setAttribute("data-theme", "dark");
+      const message = {
+          type: 'set-theme',
+          theme: 'github-dark'
+        };
+        var utterances = document.querySelector('iframe');
+        utterances.contentWindow.postMessage(message, 'https://utteranc.es');
     } else {
       initTheme(state);
     }
